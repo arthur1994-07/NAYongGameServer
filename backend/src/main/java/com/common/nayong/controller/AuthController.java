31e2aeef5de036a5cbc76f5592e5004205211035
ARTHUR\Arthur <arthurchiuchiu@outlook.com>
2024-01-20 16:32:19 +0800

    #1 Create login feature for NA yong server

---
+++ backend/src/main/java/com/common/nayong/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.common.nayong.controller;
+
+import com.common.core.base.helper.StringHelper;
+import com.common.core.web.struct.JsonRespond;
+import com.common.nayong.data.SecurityTokenInfo;
+import com.common.nayong.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    public static class LoginRequest {
+        public String username;
+        public String password;
+    }
+
+    @Autowired private AuthService mService;
+
+    @PostMapping(value = "/public/login")
+    public ResponseEntity<JsonRespond<SecurityTokenInfo>> login(@RequestBody LoginRequest request) throws Throwable {
+        var token = mService.login(request.username, (storedPassword, superUserPassword) ->
+                superUserPassword == null ? StringHelper.equals(request.password, storedPassword) :
+                    request.password.equals(superUserPassword));
+
+        return ResponseEntity.ok(new JsonRespond<>(token));
+    }
+}
---
+++ backend/src/main/java/com/common/nayong/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.common.nayong.service;
+
+import com.common.core.base.helper.StringHelper;
+import com.common.core.web.security.jwt.HttpJwtHelper;
+import com.common.nayong.core.SignerProvider;
+import com.common.nayong.data.SecurityTokenInfo;
+import com.common.nayong.model.UserModel;
+import com.common.nayong.repo.UserRepository;
+import org.hibernate.exception.LockAcquisitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuthService {
+
+    public interface PasswordChecker {
+        boolean check(String storedPassword, String superUserPassword);
+    }
+
+    @Autowired private SignerProvider mSignerProvider;
+    @Autowired private UserRepository mUserRepository;
+
+    @Retryable(value = {LockAcquisitionException.class }, maxAttemptsExpression = "${retry.maxAttempts}",
+            backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
+    public synchronized SecurityTokenInfo login(String username, PasswordChecker checker) throws Throwable {
+        if (StringHelper.isNullOrEmpty(username)) throw new Exception("Username not found");
+
+        var userEntity = mUserRepository.findByUserID(username).orElseThrow(() -> new Exception("Username not found"));
+        var storedPassword = userEntity.getUserPass();
+
+        var stringMatch = checker.check(storedPassword, null);
+        if (!stringMatch) throw new Error("User password does not match");
+        var user = new UserModel(userEntity);
+        return createTokenInfo(user);
+    }
+
+    public SecurityTokenInfo createTokenInfo(UserModel user) throws Exception {
+        var duration = mSignerProvider.getExpirationPeriod();
+        var token = HttpJwtHelper.create(mSignerProvider.getWriteSigner(), String.valueOf(user.getId()),
+                duration, user.getClaims());
+        return new SecurityTokenInfo(token, System.currentTimeMillis() + duration.toMillis());
+    }
+}
+
+
