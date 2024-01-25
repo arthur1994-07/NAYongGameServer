466a5da72e556694310a4ceb0cb373afd8223e66
ARTHUR\Arthur <arthurchiuchiu@outlook.com>
2024-01-25 18:02:17 +0800

    frontend: project structure setup

---
+++ frontend/src/config/config.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let production = import.meta.env.VITE_NODE_ENV === 'production';
+let prefix_base = import.meta.env.VITE_BASE_URL == null ? null : import.meta.env.VITE_BASE_URL;
+
+const hostname = window.location.hostname
+const httpType = window.location.protocol
+const port = production ? (httpType == "http:" ? 6666 : 8443) : 6666
+
+let config = {
+	$api_url: `${httpType}//${hostname}:${port}`,
+	$prefix_base: prefix_base,
+    $redirect_host: `${httpType}//${window.location.host}`,
+	$prefix_base: prefix_base,
+}
+
+
+export { config }
