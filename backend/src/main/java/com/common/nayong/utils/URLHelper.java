bf8436f6a28fd631506ab8a35bdaaa05299d55ae
ARTHUR\Arthur <arthurchiuchiu@outlook.com>
2024-02-12 00:57:26 +0800

    backend: update paypal dependency

---
+++ backend/src/main/java/com/common/nayong/utils/URLHelper.java
@@ -0,0 +1,10 @@
+package com.common.nayong.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class URLHelper {
+
+    public static String getBaseUrl(HttpServletRequest request) {
+        return null;
+    }
+}
