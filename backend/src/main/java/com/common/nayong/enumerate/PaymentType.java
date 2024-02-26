53ae720d6c0a9a39619a2fe9e8d04c2ebd1c00fd
ARTHUR\Arthur <arthurchiuchiu@outlook.com>
2024-02-26 16:57:29 +0800

    backend: update payment type

--- backend/src/main/java/com/common/nayong/enumerate/PaymentType.java
+++ backend/src/main/java/com/common/nayong/enumerate/PaymentType.java
@@ -2,13 +2,15 @@ package com.common.nayong.enumerate;
 
 public enum PaymentType {
     creditCard(1, "credit_card"),
-    paypal(2, "paypal");
+    paypal(2, "paypal"),
+    applePay(3, "apple_pay"),
+    googlePay(4, "google_pay");
 
-    public final int code;
+    public final int key;
     public final String name;
 
-    PaymentType(int code, String name) {
-        this.code = code;
+    PaymentType(int key, String name) {
+        this.key = key;
         this.name = name;
     }
 
