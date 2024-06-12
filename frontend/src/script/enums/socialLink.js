1826acc4344065352e3459cede620dea15064430
Arthur <arthurchiuchiu@outlook.com>
2024-06-12 08:38:58 +0100

    frontend: assets folder restructuring

---
+++ frontend/src/script/enums/socialLink.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export const facebook = "Facebook"
+export const x = "X"
+export const instagram = "Instagram"
+export const line = "Line"
+export const whatsapp = "WhatsApp"
+
+export const socialLinkList = [
+	{ key: 1, name: "Facebook" , picUrl: new URL('../../assets/social_media/facebook.png', import.meta.url).href },
+	{ key: 2, name: "X" , picUrl: new URL('../../assets/social_media/x.png', import.meta.url).href },
+	{ key: 3, name: "Instagram" , picUrl: new URL('../../assets/social_media/instagram.png', import.meta.url).href },
+	{ key: 4, name: "Line" , picUrl: new URL('../../assets/social_media/line.png', import.meta.url).href },
+	{ key: 5, name: "WhatsApp" , picUrl: new URL('../../assets/social_media/whatsapp.png', import.meta.url).href }
+]
---
+++ frontend/src/views/components/ShareLinkData.vue
@@ -0,0 +1,26 @@
+<template>
+	<meta property="og:url" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:image" content="" />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        url: { type: String, default : "" },
+        type: { type: any , default : "" },
+
+    },
+    emits: [
+
+    ],
+    setup() {
+        
+        return { }
+    }
+})
+</script>
